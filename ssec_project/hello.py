713564bacfba99aa4bb19b7a4eb5f5ad29fac775
Landung 'Don' Setiawan <landungs@uw.edu>
2023-01-18 10:53:40 -0800

    Initial package setup

---
+++ ssec_project/hello.py
@@ -0,0 +1,14 @@
+"""hello.py
+Simple hello module for example
+"""
+
+def say_hello() -> str:
+    """
+    Function that prints out a short sentence.
+
+    Returns
+    -------
+    str
+        "Hello world"
+    """
+    return "Hello world."
