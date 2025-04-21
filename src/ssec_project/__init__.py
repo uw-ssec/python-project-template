a07439ace213698fa23b625969a5898c2d50895a
Don Setiawan <landungs@uw.edu>
2025-04-21 11:58:28 -0700

    Update __init__.py

--- src/ssec_project/__init__.py
+++ src/ssec_project/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from .version import version as __version__
+from .version import version as __version__  # type: ignore[import-not-found]
 
 __all__ = ["__version__"]
