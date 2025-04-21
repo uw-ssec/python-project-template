0a1b080f0626d8a4fa5d63a069228a94f323b167
Landung 'Don' Setiawan <landungs@uw.edu>
2025-04-21 11:55:19 -0700

    fix: Fix pre-commit errors

--- src/ssec_project/__init__.py
+++ src/ssec_project/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from .version import version as __version__  # type: ignore[import-not-found]
+from .version import version as __version__
 
 __all__ = ["__version__"]
